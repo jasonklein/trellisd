a1a6152010c0f3daf3e2aa0af683034c74cca035
jasonklein <jasonklein@gmail.com>
2014-06-06 16:19:16 +0100

    Add some seed data, add faker gem for more seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# users = User.create([
+#   {birthday:'1982-06-04', city:'London', postcode:'E8', image:'http://www.example.com', email:'monica@example.com', first_name:'Monica', last_name:'Geller', bio:''},
+#   {birthday:'1982-06-04', city:'London', postcode:'E8', image:'http://www.example.com', email:'phoebe@example.com', first_name:'Phoebe', last_name:'Buffay', bio:''},
+#   {birthday:'1982-06-04', city:'London', postcode:'E8', image:'http://www.example.com', email:'rachel@example.com', first_name:'Rachel', last_name:'Green', bio:''},
+#   {birthday:'1982-06-04', city:'London', postcode:'E8', image:'http://www.example.com', email:'ross@example.com', first_name:'Ross', last_name:'Geller', bio:''},
+#   {birthday:'1982-06-04', city:'London', postcode:'E8', image:'http://www.example.com', email:'joey@', first_name:'Joey', last_name:'Trib', bio:''},
+#   {birthday:'1982-06-04', city:'London', postcode:'E8', image:'http://www.example.com', email:'', first_name:'Chandler', last_name:'Bing', bio:''}
+#   ])
