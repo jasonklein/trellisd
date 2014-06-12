14743e2e2bdd4aba258625f7c7908f5726cdb2cb
jasonklein <jasonklein@gmail.com>
2014-06-12 13:06:47 +0100

    Add devise views to customize; refactor display_session_control_links view helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,10 @@
 module ApplicationHelper
 
   def display_session_control_links
-    current_uri = request.env['PATH_INFO']
-    if current_user
+    if current_user 
       link_to "Sign Out", destroy_user_session_path, method: :delete
-    elsif current_uri != new_user_session_path
-      link_to "Sign In or Sign Up", new_user_session_path
+    else
+      nil
     end
   end
 end
