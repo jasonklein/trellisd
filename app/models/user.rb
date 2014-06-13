d627d404e9626ec9c03bbbf23ae0a0de8c733fa6
jasonklein <jasonklein@gmail.com>
2014-06-13 12:39:38 +0100

    Refactor connections finding methods in User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,34 +28,44 @@ class User < ActiveRecord::Base
     self.made_connections + self.received_connections
   end
 
+  def connections_hash
+    @connections_hash = {}
+  end
+
+
   # Fill array with ids of users to whom a particular user
   # is connected, returning that array
 
-  def ids_of_primary_connections
-    connections_ids = []
-    self.made_connections.each do |connection|
-      connections_ids << connection.connectee_id
+  def primary_connections
+    if !@primary_connections_hash
+      connections_ids = []
+      self.made_connections.each do |connection|
+        connections_ids << connection.connectee_id
+      end
+      self.received_connections.each do |connection|
+        connections_ids << connection.connecter_id
+      end
+      @primary_connections_hash = {primary: connections_ids.uniq}
     end
-    self.received_connections.each do |connection|
-      connections_ids << connection.connecter_id
-    end
-    connections_ids.uniq
+    connections_hash.merge @primary_connections_hash
+    @primary_connections_hash || {}
   end
 
   # Fill array with ids of users to whom a particular user
   # is secondarily connected/connected by another user
   # returning that array
 
-  def ids_of_secondary_connections
-    primary_connections_ids = self.ids_of_primary_connections
-    secondary_connections_ids = []
-    primary_connections_ids.each do |connection_id|
-      connection = User.find(connection_id)
-      connections_ids = connection.ids_of_primary_connections
-      secondary_connections_ids += connections_ids
+  def secondary_connections
+    if !@secondary_connections_hash
+      secondary_connections_ids = []
+      primary_connections[:primary].each do |connection_id|
+        connection = User.find(connection_id)
+        secondary_connections_ids += connection.primary_connections[:primary]
+      end
+      secondary_connections_ids -= primary_connections[:primary]
+      @secondary_connections_hash = {secondary: secondary_connections_ids.uniq}
     end
-    secondary_connections_ids -= primary_connections_ids
-    secondary_connections_ids.uniq
+    @secondary_connections_hash || {}
   end
 
   # Fill array with ids of users to whom a particular user
@@ -65,22 +75,18 @@ class User < ActiveRecord::Base
   # See comment above ids_of_all_connections (below) to understand
   # the optional array parameter.
 
-  def ids_of_tertiary_connections(all=[])
-    primary_connections_ids = self.ids_of_primary_connections
-    secondary_connections_ids = self.ids_of_secondary_connections
-    primary_and_secondary_connections_ids = primary_connections_ids + secondary_connections_ids
-    tertiary_connections_ids = []
-    secondary_connections_ids.each do |connection_id|
-      connection = User.find(connection_id)
-      connections_ids = connection.ids_of_primary_connections
-      tertiary_connections_ids += connections_ids
+  def tertiary_connections(all=[])
+    if !@tertiary_connections_hash
+      primary_and_secondary_connections_ids = primary_connections[:primary] + secondary_connections[:secondary]
+      tertiary_connections_ids = []
+      secondary_connections[:secondary].each do |connection_id|
+        connection = User.find(connection_id)
+        tertiary_connections_ids += connection.primary_connections[:primary]
+      end
+      tertiary_connections_ids -= primary_and_secondary_connections_ids
+      @tertiary_connections_hash = {tertiary: tertiary_connections_ids.uniq}
     end
-    tertiary_connections_ids -= primary_and_secondary_connections_ids
-    if all.empty?
-      tertiary_connections_ids.uniq
-    else
-      primary_and_secondary_connections_ids + tertiary_connections_ids.uniq
-    end
+    @tertiary_connections_hash || {}
   end
 
   # As the tertiary connections method also finds primary
@@ -90,9 +96,10 @@ class User < ActiveRecord::Base
   # is set to return all three levels when called by the
   # all_connections function
 
-  def ids_of_all_connections
-    all_connections_ids = ids_of_tertiary_connections([1])
-  end
+  # def ids_of_all_connections
+  #   primary_connections_hash, secondary_connections_hash, tertiary_connections_hash = ids_of_tertiary_connections([1])
+  #   primary_connections_hash, secondary_connections_hash, tertiary_connections_hash
+  # end
 
   # TODO: Determine which method to call to get posts by user_id
 
