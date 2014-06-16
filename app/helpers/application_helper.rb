a509140c87bfe584a8a4910693cc85b1c3b2e36b
jasonklein <jasonklein@gmail.com>
2014-06-16 18:14:35 +0100

    Style navigation bar and add media breakpoints for a more responsive design.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,11 @@ module ApplicationHelper
   def display_session_control_links
     current_uri = request.env['PATH_INFO']
     if current_user
-      link_to "Sign Out", destroy_user_session_path, method: :delete
+      link_to "Sign Out", destroy_user_session_path, method: :delete, class: "session_control"
     elsif current_uri == new_user_session_path
-      link_to "Sign Up", new_user_registration_path
+      link_to "Sign Up", new_user_registration_path, class: "session_control"
     else
-      link_to "Sign In", new_user_session_path
+      link_to "Sign In", new_user_session_path, class: "session_control"
     end
   end
 end
