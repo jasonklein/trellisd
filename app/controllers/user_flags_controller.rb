307764f88c5f14388be0090ff1c13fdbc7e6e8da
jasonklein <jasonklein@gmail.com>
2014-06-25 22:28:42 +0100

    Tweak successful flagging notice.

--- app/controllers/user_flags_controller.rb
+++ app/controllers/user_flags_controller.rb
@@ -7,7 +7,7 @@ class UserFlagsController < ApplicationController
     user_flag.flagger_id = params[:flagger_id].to_i
     user_flag.flagged_id = params[:flagged_id].to_i
     if user_flag.save
-      redirect_to user_home_path(current_user), notice: "User flagged. Thank you."
+      redirect_to user_home_path(current_user), notice: "User flagged. We will review."
     else
       redirect_to user_home_path(current_user), notice: user_flag.errors_for_redirect
     end
