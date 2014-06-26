38ccad7f3ebed18a67b5970128f60dcf6b5a510c
jasonklein <jasonklein@gmail.com>
2014-06-26 21:26:01 +0100

    Adjust error notices in flags models.

--- app/models/user_flag.rb
+++ app/models/user_flag.rb
@@ -8,15 +8,15 @@ class UserFlag < ActiveRecord::Base
   validate :flagger_cannot_be_flagged, :flagger_cannot_flag_flagged_more_than_once
 
   def flagger_cannot_be_flagged
-    errors.add(:user, "cannot flag self") if flagger_id == flagged_id
+    errors.add(:base, "you cannot flag yourself") if flagger_id == flagged_id
   end
 
   ### Defined validation below, rather than a scoped validation_of_uniqueness
   ### to have the more semantic error message.
 
   def flagger_cannot_flag_flagged_more_than_once
-    user_flags = UserFlag.where(flagger_id: flagger_id, flagged_id: flagged_id)
-    errors.add(:user, "has already been flagged by you") if user_flags.any?
+    previous_flags = UserFlag.where(flagger_id: flagger_id, flagged_id: flagged_id)
+    errors.add(:base, "you have already flagged this user") if previous_flags.any?
   end
 
   def errors_for_redirect
