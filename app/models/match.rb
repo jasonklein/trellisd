068e86560dd73497b65cdf7e4a42ca0b90a93907
jasonklein <jasonklein@gmail.com>
2014-06-27 15:45:26 +0100

    Change default order of matches to by keyword coverage.

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,6 +3,6 @@ class Match < ActiveRecord::Base
   belongs_to :matching, class_name: "Post"
   attr_accessible :keyword_coverage, :post_id, :matching_id
 
-  default_scope order('created_at DESC')
+  default_scope order('keyword_coverage DESC')
   
 end
