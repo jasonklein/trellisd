efa22ba1dc1fe8dd1c4744dc4ff177b323331adc
jasonklein <jasonklein@gmail.com>
2014-06-27 11:37:14 +0100

    Delete unnecessary code w/r/t class name for post association.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,5 +1,5 @@
 class Match < ActiveRecord::Base
-  belongs_to :post, class_name: "Post"
+  belongs_to :post
   belongs_to :matching, class_name: "Post"
   attr_accessible :keyword_coverage, :post_id, :matching_id
 
