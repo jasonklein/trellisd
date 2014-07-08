5d8257a186fafa5fdc7df43b1768c452d52f7c11
jasonklein <jasonklein@gmail.com>
2014-07-08 23:35:02 +0100

    Set delivery method to letter_opener gem in development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,9 @@ Trellisd::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  # Have letters opened by Letter Opener gem
+  config.action_mailer.delivery_method = :letter_opener
+
   # For Devise
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
