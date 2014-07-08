ef2db000e8b0634a38484cfe08cbdedd7ba8e0a6
jasonklein <jasonklein@gmail.com>
2014-07-09 00:50:45 +0100

    Add notify_user_of_connection_request method and refactor reset_password_instructions method.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,17 @@ class UserMailer < Devise::Mailer
   include Devise::Controllers::UrlHelpers
 
   def reset_password_instructions(record, token, opts={})
-    attachments.inline['trellisd_email_image'] = File.read 'app/assets/images/trellisd-logo-100.png'
+    set_inline_trellisd_logo
     super
+  end
+
+  def notify_user_of_connection_request(user, connecter)
+    @user = user
+    attachments.inline['trellisd_email_image'] = File.read 'app/assets/images/trellisd-logo-100.png'
+    mail to: @user.email, subject: connecter.name + 'wants to connect!'
+  end
+
+  def set_inline_trellisd_logo
+    attachments.inline['trellisd_email_image'] = File.read 'app/assets/images/trellisd-logo-100.png'
   end
 end
