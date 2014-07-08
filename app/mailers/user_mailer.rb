0e5416a24ac5f9252c99b57c14964dbb7014bc4b
jasonklein <jasonklein@gmail.com>
2014-07-09 00:32:49 +0100

    Customize reset_password email view; add trellisd logo to include inline in email.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,9 @@ class UserMailer < Devise::Mailer
   default reply_to: "trellisdapp@gmail.com"
 
   include Devise::Controllers::UrlHelpers
+
+  def reset_password_instructions(record, token, opts={})
+    attachments.inline['trellisd_email_image'] = File.read 'app/assets/images/trellisd-logo-100.png'
+    super
+  end
 end
