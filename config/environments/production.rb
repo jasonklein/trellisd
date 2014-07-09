50dd4c745a2e79d8ccc747ef6b1a7510f897968f
jasonklein <jasonklein@gmail.com>
2014-07-09 03:34:54 +0100

    Add Gmail configuration to production.rb.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,15 @@ Trellisd::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'trellisd.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'trellisd.herokuapp.com',
+    user_name:            'trellisdapp',
+    password:             ENV['TRELLISD_EMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
   # Enable threaded mode
   # config.threadsafe!
